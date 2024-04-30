347547845af161824a7c1918c67d953b4f6dbaeb
lahham392 <yousefallahham@gmail.com>
2024-04-30 08:11:52 +0000

    edting sidebar to add items in a new line

--- frontend-react-js/src/components/DesktopSidebar.js
+++ frontend-react-js/src/components/DesktopSidebar.js
@@ -40,9 +40,8 @@ export default function DesktopSidebar(props) {
       <footer>
         <a href="#">About</a>
         <a href="#">Terms of Service</a>
-        <a href="#">Privacy Policy</a>
-        <br>
-        <a herf="#">More ...</a>
+        <a href="#">Privacy Policy</a><br>
+        <a herf="#">More ...</a><br>
         <a herf="#">© 2024 Lahham Corp.</a>
       </footer>
     </section>
